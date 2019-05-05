ed98b0f0f4649c2c19834233430a410c0bdb20d5
Eduardo Henrique Rotundaro <eduardorotundaro@gmail.com>
2019-05-05 15:27:30 -0300

    Udpate'

---
+++ src/components/App/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Container from '../layout/Container'
+import NavBar from '../templates/partials/NavBar'
+import Routes from '../../routes'
+
+import { NAV_ITENS } from '../../constants/content'
+
+export default props => (
+    <Router>
+        <NavBar itens={ NAV_ITENS }/>
+        <Container>
+            <Routes/>
+        </Container>
+    </Router>
+)
